be7dc7bf901aea9b4c9950c0294c57d4723290f5
hongngoc <hongngoctran0@gmail.com>
2019-02-14 09:27:12 +0700

    add timelife

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'timelines',
+    component: TimelinesComponent
+
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelinesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
